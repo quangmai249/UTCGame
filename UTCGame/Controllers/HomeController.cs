0c8b8b7bedbcc7091e21a5ee1db409f70af3294b
Mai Quang <quangmai249@gmail.com>
2024-08-18 08:54:34 +0700

    Front end Support

--- UTCGame/Controllers/HomeController.cs
+++ UTCGame/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
+using UTCGame.Data;
 using UTCGame.Models;
 
 namespace UTCGame.Controllers
@@ -7,10 +9,12 @@ namespace UTCGame.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext applicationDB)
         {
             _logger = logger;
+            _context = applicationDB;
         }
 
         public IActionResult Index()
@@ -20,7 +24,37 @@ namespace UTCGame.Controllers
 
         public IActionResult Privacy()
         {
+            var media = _context.FolderMediaModel.Where(x => x.IsActive && x.FolderMediaID.Equals(Guid.Parse("368BF5FE-F405-45C3-9EE0-809C0E7A0526")));
+            if (!media.IsNullOrEmpty())
+            {
+                string path = "wwwroot/media/Privacy/";
+                var file = Directory.GetFiles(path, "*.pdf", SearchOption.TopDirectoryOnly);
+                if (file != null)
+                {
+                    ViewBag.Path = file.FirstOrDefault();
+                }
+                else
+                {
+                    ViewBag.Path = "We are updating the privacy and policy!";
+                }
+            }
             return View();
+        }
+
+        public IActionResult Support(string? _support)
+        {
+            if (_support != null && _support.Equals("Contact"))
+            {
+                var _Contact = _context.EmployeeModel.Where(x => x.IsEmployeeActive && x.RoleID.Equals(Guid.Parse("3BDC247F-9F53-4808-8298-AA9E975DAE75"))).ToList();
+                ViewBag.Contact = _Contact;
+                return View();
+            }
+            else
+            {
+                var _FAQ = _context.FAQ.Where(x => x.IsActive).ToList();
+                ViewBag.FAQ = _FAQ;
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
